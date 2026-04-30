6bc9b06a21a66d0963ab5dd6f73b957deca4ec73
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-04-30 18:12:33 -0400

    chore: initialize all pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from './HomePage.tsx';
+export { default as PortfolioPage } from './PortfolioPage.tsx';
+export { default as CreativeLifePage } from './CreativeLifePage.tsx';
+export { default as ImpactPage } from './ImpactPage.tsx';
+export { default as ContactPage } from './ContactPage.tsx';
